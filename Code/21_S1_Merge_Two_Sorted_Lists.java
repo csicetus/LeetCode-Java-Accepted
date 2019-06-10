9e36d95eeb86ab11a1e22afaaf073e7199647057
csicetus <1577840083@qq.com>
2019-06-10 05:37:51 -0400

    Create 21_S1_Merge_Two_Sorted_Lists.java

---
+++ Code/21_S1_Merge_Two_Sorted_Lists.java
@@ -0,0 +1,32 @@
+/*
+ * 21. Merge Two Sorted Lists solution 1
+ * 2019-06-10  Runtime: 0 ms
+ * Target: Merge two sorted linked lists and return it as a new list.
+ * Difficulty：Easy
+ * Classification：Linked List
+ * Algorithm: => Iteratively.
+ * Time complexity: O(m + n), Space complexity: O(1)
+ */
+ 
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 != null && l2 != null){
+            if (l1.val > l2.val){
+                ListNode temp = l1;
+                l1 = l2;
+                l2 = temp;
+            }
+            l1.next = mergeTwoLists(l1.next, l2);
+        }
+        return (l1 != null) ? l1 : l2;
+    }
+}
