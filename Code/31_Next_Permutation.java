489f4b910401f86b7e92bba8ca3bd5165607353b
csicetus <1577840083@qq.com>
2019-06-16 22:38:56 -0400

    Create 31_Next_Permutation.java

---
+++ Code/31_Next_Permutation.java
@@ -0,0 +1,46 @@
+/*
+ * 31. Next Permutation
+ * 2019-06-16  Runtime: 0 ms
+ * Target: Implement next permutation.
+ * Difficulty：Medium
+ * Classification：Array
+ * Algorithm: => Look for the first smaller number x from the back, find the first number y larger than x from the number 
+ *               after this number, exchange, and then reverse numbers after the number y.
+ * Time complexity: O(n), Space complexity: O(1)
+ */
+ 
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        int j = i + 1;
+        if (j > 0) {
+            while (i >= 0 && nums[i] >= nums[i + 1] ) {
+                i--;
+            }
+            if (i < 0) {
+                reverse(nums, 0);
+                return;
+            }
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+            reverse(nums, i + 1);
+        }
+    }
+    
+    public void swap(int[] nums, int s1, int s2) {
+        int temp = nums[s1];
+        nums[s1] = nums[s2];
+        nums[s2] = temp;
+    }
+    
+    public void reverse(int[] nums, int k) {
+        int m = k, n = nums.length - 1;
+        while (m < n) {
+            swap(nums, m, n);
+            m++;
+            n--;
+        }        
+    }
+}
