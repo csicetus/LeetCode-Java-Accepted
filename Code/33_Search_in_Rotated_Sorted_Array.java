344beef26b60177ae3144e84c56b1af41f53876d
csicetus <1577840083@qq.com>
2019-06-17 23:06:29 -0400

    Update 33_Search_in_Rotated_Sorted_Array.java

--- Code/33_Search_in_Rotated_Sorted_Array.java
+++ Code/33_Search_in_Rotated_Sorted_Array.java
@@ -7,43 +7,100 @@
  
   /*
  * Solution 1
- * 2019-06-15  Runtime: 0 ms
- * Algorithm: => Brute Force.
- * Time complexity: O(n*logn). Space complexity: O(n).
+ * 2019-06-17  Runtime: 0 ms
+ * Algorithm: => Search by bias.
+ * Time complexity: O(logn). Space complexity: O(1).
  */
  
  class Solution {
-    public void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
-        int j = i + 1;
-        if (j > 0) {
-            while (i >= 0 && nums[i] >= nums[i + 1] ) {
-                i--;
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int n = nums.length;
+        int end = n - 1;
+        
+        // find min number index
+        while (start < end) {
+            int mid = Math.round((start + end) / 2);
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
             }
-            if (i < 0) {
-                reverse(nums, 0);
-                return;
+            else {
+                end = mid;
             }
-            while (j >= 0 && nums[j] <= nums[i]) {
-                j--;
-            }
-            swap(nums, i, j);
-            reverse(nums, i + 1);
         }
+        
+        // find max number index
+        // while (start < end) {
+        //     int mid = (start + end) / 2;
+        //     if (nums[mid] < nums[start]) {
+        //         end = mid + 1;
+        //     }
+        //     else {
+        //         start = mid;
+        //     }
+        // }
+        
+        //int bias = (start + n) - (n - 1);
+        
+        // (pos + bias) % n = index
+        // (n - 1 + bias) % n = start
+        int bias = start;
+        
+        start = 0;
+        end = n - 1;
+        
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int mid_index = (mid + bias) % n;
+            System.out.println(mid_index);
+            if (target == nums[mid_index]) {
+                return mid_index;
+            }
+            if (target > nums[mid_index]) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return -1;
     }
-    
-    public void swap(int[] nums, int s1, int s2) {
-        int temp = nums[s1];
-        nums[s1] = nums[s2];
-        nums[s2] = temp;
-    }
-    
-    public void reverse(int[] nums, int k) {
-        int m = k, n = nums.length - 1;
-        while (m < n) {
-            swap(nums, m, n);
-            m++;
-            n--;
-        }        
+}
+
+
+/*
+ * Solution 2
+ * 2019-06-17  Runtime: 0 ms
+ * Algorithm: => Search by make the other part inf or -inf.
+ * Time complexity: O(logn). Space complexity: O(1).
+ */
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[0] == target) {
+                return 0;
+            }
+            
+            if ((nums[mid] < nums[0]) != (target < nums[0])) {
+                nums[mid] = (target > nums[0]) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            
+            if (nums[mid] > target) {
+                hi = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                lo = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+        
     }
 }
