97af160cdd8809c55caee4f856d375c590cfb6d0
csicetus <1577840083@qq.com>
2019-06-21 03:45:21 -0400

    Update 17_S2_Letter_Combinations_of_a_Phone_Number.java

--- Code/17_S2_Letter_Combinations_of_a_Phone_Number.java
+++ Code/17_S2_Letter_Combinations_of_a_Phone_Number.java
@@ -1,9 +1,13 @@
 /*
  * 17. Letter Combinations of a Phone Number solution 2
- * 2019-06-08  Runtime: 0 ms
  * Target: Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
  * Difficulty：Medium
  * Classification：String, Backtracking
+ */
+
+/*
+ * Solution 1
+ * 2019-06-08  Runtime: 0 ms
  * Algorithm: => Recursive
  *               If digits == "23", 
  *               first layer recursive: combination("a", "23", 1, ""), combination("b", "23", 1, ""), combination("c", "23", 1, "");
@@ -35,3 +39,33 @@ class Solution {
         }
     }
 }
+
+
+/*
+ * Solution 2
+ * 2019-06-08  Runtime: 0 ms
+ * Algorithm: => Queue iteration
+ *               If digits == "236", first loop: res = [a, b, c], second loop: res = [b, c, ad, af, ae]...
+ *               Until i == 2, res = [ad, ae, af, bd, be, bf, cd, ce, cf]
+ */
+
+ class Solution {
+    public List<String> letterCombinations(String digits) {
+        LinkedList<String> res = new LinkedList<>();
+        if(digits.isEmpty()) return res;
+        String[] letters = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
+        res.add("");
+        for (int i = 0; i < digits.length(); i++){
+            int j = Character.getNumericValue(digits.charAt(i));
+            while (res.peek().length() == i){
+                String first = res.remove();
+                for (char ch : letters[j].toCharArray()){
+                    res.add(first + ch);
+                }
+            }
+        }
+        return res;
+    }
+}
+ 
