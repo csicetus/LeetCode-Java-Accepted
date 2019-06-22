fedb1d9ae78049cd781273cf4075da3a1c7bb401
csicetus <1577840083@qq.com>
2019-06-22 05:20:13 -0400

    Update 39_Combination_Sum.java

--- Code/39_Combination_Sum.java
+++ Code/39_Combination_Sum.java
@@ -32,3 +32,48 @@
         }
     }
 }
+
+
+/*
+ * Solution 2
+ * 2019-06-22  Runtime: 8 ms
+ * Algorithm: => DP.
+ */
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<List<Integer>>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        if (candidates[0] > target) {
+            return new ArrayList<List<Integer>>();
+        }
+        
+        for (int i = 0; i <= target; i++) {
+            List<List<Integer>> res_i = new ArrayList<>();
+            res.add(i, res_i);
+        }
+        
+        for (int j = 0; j < candidates.length; j++) {
+            for (int sum = candidates[j]; sum <= target; sum++) {
+                List<List<Integer>> res_sum = res.get(sum);
+                List<List<Integer>> res_sub = res.get(sum - candidates[j]);
+                
+                if (candidates[j] == sum) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(candidates[j]);
+                    res_sum.add(temp);
+                }
+                
+                if (res_sub.size() > 0) {
+                     for (int k = 0; k < res_sub.size(); k++) {
+                         List<Integer> temp = new ArrayList<>(res_sub.get(k));
+                         temp.add(candidates[j]);
+                         res_sum.add(temp);
+                     }
+                }
+            }
+        }
+        
+        return res.get(target);
+    }
+}
