7847bf5d4ab3f7a44e27b1fccb27c6f98b0c37de
csicetus <1577840083@qq.com>
2019-06-22 23:47:13 -0400

    Update 48_Rotate_Image.java

--- Code/48_Rotate_Image.java
+++ Code/48_Rotate_Image.java
@@ -35,7 +35,23 @@
 
 /*
  * Solution 2
- * 2019-06-23  Runtime: 322 ms
- * Algorithm: => Compare one by one.
- * Time complexity: O(k*n). Space complexity: O(1).
+ * 2019-06-22  Runtime: 0 ms
+ * Algorithm: => Rotate.
+ * Time complexity: O(n^2). Space complexity: O(1).
  */
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i - 1; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-j-1][i];
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+                matrix[j][n-i-1] = tmp;
+            }
+        }
+    }
+}
