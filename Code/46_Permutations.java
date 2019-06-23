2ef9810e484b528595cf2902b017482d41586fdf
csicetus <1577840083@qq.com>
2019-06-22 22:48:08 -0400

    Update 46_Permutations.java

--- Code/46_Permutations.java
+++ Code/46_Permutations.java
@@ -37,7 +37,39 @@
  
 /*
  * Solution 2
- * 2019-06-22  Runtime: 322 ms
- * Algorithm: => Compare one by one.
+ * 2019-06-22  Runtime: 1 ms
+ * Algorithm: => Recursion, same as DP.
  * Time complexity: O(k*n). Space complexity: O(1).
  */
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        upset(nums, 0, res);
+        return res;
+    }
+    
+    private void upset(int[] nums, int begin, List<List<Integer>> res) {
+        if (begin == nums.length) {
+            List<Integer> templist = new ArrayList<>();
+            for (int i = 0; i < begin; i++) {
+                templist.add(nums[i]);
+            }
+            res.add(new ArrayList<>(templist));
+            return;
+        }
+        
+        for (int i = begin; i < nums.length; i++) {
+            swap(nums, i, begin);
+            upset(nums, begin + 1, res);
+            swap(nums, i, begin);
+        }
+    }
+    
+    private void swap(int[] nums, int i, int begin) {
+        int temp = nums[i];
+        nums[i] = nums[begin];
+        nums[begin] = temp;
+    }
+    
+}
