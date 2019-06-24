d445510be37c97dce5f017537cff6d6be9431528
csicetus <1577840083@qq.com>
2019-06-23 23:54:55 -0400

    Update 19_Remove_Nth_Node_From_End_of_List.java

--- Code/19_Remove_Nth_Node_From_End_of_List.java
+++ Code/19_Remove_Nth_Node_From_End_of_List.java
@@ -1,5 +1,5 @@
 /*
- * 19. Remove Nth Node From End of List solution 1
+ * 19. Remove Nth Node From End of List
  * Target: Given a linked list, remove the n-th node from the end of list and return its head.
  * Difficulty：Medium
  * Classification：Linked List, Two Pointers
