f1114fced66c8562c5ee98727615a65d25e1619a
csicetus <1577840083@qq.com>
2019-06-23 22:53:37 -0400

    Update 49_Group_Anagrams.java

--- Code/49_Group_Anagrams.java
+++ Code/49_Group_Anagrams.java
@@ -35,7 +35,34 @@
 
 /*
  * Solution 2
- * 2019-06-15  Runtime: 322 ms
- * Algorithm: => Compare one by one.
+ * 2019-06-23  Runtime: 7 ms
+ * Algorithm: => Arrange key as factors of prime numbers.
  * Time complexity: O(k*n). Space complexity: O(1).
+ * Note: The basic theorem of arithmeti => each natural number greater than 1, either itself is a prime number, 
+ *       or can be written as a product of more than two prime numbers, and after these quality factors are arranged by size, 
+ *       there is only one way to write.
  */
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<Integer, List<String>> map = new HashMap<>();
+        
+        int[] prime = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103 };
+        for (String s : strs) {
+            int key = 1;
+            char[] ch = s.toCharArray();
+            for (char sc : ch) {
+                key *= prime[sc - 'a'];
+            }
+            if (map.containsKey(key)) {
+                map.get(key).add(s);
+            }
+            else {
+                List<String> tmp = new ArrayList<>();
+                tmp.add(s);
+                map.put(key, tmp);
+            }
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
