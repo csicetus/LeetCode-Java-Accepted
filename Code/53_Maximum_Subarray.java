d370b2db148006e43e5b4d870e388bf97e28a7db
csicetus <1577840083@qq.com>
2019-06-24 23:38:57 -0400

    Update 53_Maximum_Subarray.java

--- Code/53_Maximum_Subarray.java
+++ Code/53_Maximum_Subarray.java
@@ -27,7 +27,39 @@ class Solution {
 
 /*
  * Solution 2
- * 2019-06-24  Runtime: 322 ms
- * Algorithm: => Compare one by one.
- * Time complexity: O(k*n). Space complexity: O(1).
+ * 2019-06-24  Runtime: 1 ms
+ * Algorithm: => Divide and Conquer.
+ * Time complexity: O(n*logn).
  */
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        return SubArr(nums, 0, nums.length - 1);
+    }
+    
+    private int SubArr(int[] nums, int lo, int hi) {
+        if (lo == hi) {
+            return nums[lo];
+        }
+        int mid = (lo + hi) / 2;
+        return Math.max(Math.max(SubArr(nums, lo, mid), SubArr(nums, mid + 1, hi)), maxSubArrCross(nums, lo, mid, hi));
+    }
+    
+    private int maxSubArrCross(int[] nums, int lo, int mid, int hi) {
+        int sum = 0, sum_l = Integer.MIN_VALUE, sum_r = Integer.MIN_VALUE;
+        for (int i = mid; i >= lo; i--) {
+            sum += nums[i];
+            if (sum > sum_l) {
+                sum_l = sum;
+            }
+        }
+        sum = 0;
+        for (int j = mid + 1; j <= hi; j++) {
+            sum += nums[j];
+            if (sum > sum_r) {
+                sum_r = sum;
+            }
+        }
+        return sum_l + sum_r;
+    }
+}
