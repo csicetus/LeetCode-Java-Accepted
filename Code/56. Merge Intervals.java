d2ac1ff890c42557f0137794fbffa386f19b627a
csicetus <1577840083@qq.com>
2019-06-25 23:18:19 -0400

    Update 56. Merge Intervals.java

--- Code/56. Merge Intervals.java
+++ Code/56. Merge Intervals.java
@@ -8,29 +8,30 @@
 
 /*
  * Solution 1
- * 2019-06-25  Runtime: 1371 ms
+ * 2019-06-25  Runtime: ? ms
  * Algorithm: => Sort and merge.
- * Time complexity: O(n^2). Space complexity: O(n).
+ * Time complexity: O(n*logn). Space complexity: O(n).
  */
  
- class Solution {
-    private class IntervalComparator implements Comparator<Interval> {
-        @Override
-        public int compare(Interval a, Interval b) {
-            return a.start < b.start ? -1 : a.start == b.start ? 0 : 1;
-        }
+private class IntervalComparator implements Comparator<Interval> {
+    @Override
+    public int compare(Interval a, Interval b) {
+        return a.start < b.start ? -1 : a.start == b.start ? 0 : 1;
     }
-    
-    public List<Interval> merge(List<Interval> intervals) {
-        Collections.sort(intervals, new IntervalComparator());
-        
-        LinkedList<Interval> merged = new LinkedList<>();
-        
+}
+
+public List<Interval> merge(List<Interval> intervals) {
+    Collections.sort(intervals, new IntervalComparator());
+
+    LinkedList<Interval> merged = new LinkedList<Interval>();
+    for (Interval interval : intervals) {
         if (merged.isEmpty() || merged.getLast().end < interval.start) {
             merged.add(interval);
-        } else {
+        }
+        else {
             merged.getLast().end = Math.max(merged.getLast().end, interval.end);
         }
-        return merged;
     }
+
+    return merged;
 }
