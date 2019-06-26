7c25a98afe2bfcd0279d8672577ea74c1fbfef99
csicetus <1577840083@qq.com>
2019-06-25 23:41:42 -0400

    Update 56. Merge Intervals.java

--- Code/56. Merge Intervals.java
+++ Code/56. Merge Intervals.java
@@ -3,7 +3,7 @@
  * Target: Given a collection of intervals, merge all overlapping intervals.
  * Difficulty：Medium
  * Classification：Array, Greedy
- * Best solution: solution 3.
+ * Note: no correct submissons.
  */
 
 /*
@@ -34,4 +34,107 @@ public List<Interval> merge(List<Interval> intervals) {
     }
 
     return merged;
+}
+
+
+
+/*
+ * Solution 2
+ * 2019-06-25  Runtime: ? ms
+ * Algorithm: => Graph.
+ * Time complexity: O(n^2). Space complexity: O(n^2).
+ */
+ 
+class Solution {
+    private Map<Interval, List<Interval> > graph;
+    private Map<Integer, List<Interval> > nodesInComp;
+    private Set<Interval> visited;
+
+    // return whether two intervals overlap (inclusive)
+    private boolean overlap(Interval a, Interval b) {
+        return a.start <= b.end && b.start <= a.end;
+    }
+
+    // build a graph where an undirected edge between intervals u and v exists
+    // iff u and v overlap.
+    private void buildGraph(List<Interval> intervals) {
+        graph = new HashMap<>();
+        for (Interval interval : intervals) {
+            graph.put(interval, new LinkedList<>());
+        }
+
+        for (Interval interval1 : intervals) {
+            for (Interval interval2 : intervals) {
+                if (overlap(interval1, interval2)) {
+                    graph.get(interval1).add(interval2);
+                    graph.get(interval2).add(interval1);
+                }
+            }
+        }
+    }
+
+    // merges all of the nodes in this connected component into one interval.
+    private Interval mergeNodes(List<Interval> nodes) {
+        int minStart = nodes.get(0).start;
+        for (Interval node : nodes) {
+            minStart = Math.min(minStart, node.start);
+        }
+
+        int maxEnd = nodes.get(0).end;
+        for (Interval node : nodes) {
+            maxEnd= Math.max(maxEnd, node.end);
+        }
+
+        return new Interval(minStart, maxEnd);
+    }
+
+    // use depth-first search to mark all nodes in the same connected component
+    // with the same integer.
+    private void markComponentDFS(Interval start, int compNumber) {
+        Stack<Interval> stack = new Stack<>();
+        stack.add(start);
+
+        while (!stack.isEmpty()) {
+            Interval node = stack.pop();
+            if (!visited.contains(node)) {
+                visited.add(node);
+
+                if (nodesInComp.get(compNumber) == null) {
+                    nodesInComp.put(compNumber, new LinkedList<>());
+                }
+                nodesInComp.get(compNumber).add(node);
+
+                for (Interval child : graph.get(node)) {
+                    stack.add(child);
+                }
+            }
+        }
+    }
+
+    // gets the connected components of the interval overlap graph.
+    private void buildComponents(List<Interval> intervals) {
+        nodesInComp = new HashMap();
+        visited = new HashSet();
+        int compNumber = 0;
+
+        for (Interval interval : intervals) {
+            if (!visited.contains(interval)) {
+                markComponentDFS(interval, compNumber);
+                compNumber++;
+            }
+        }
+    }
+
+    public List<Interval> merge(List<Interval> intervals) {
+        buildGraph(intervals);
+        buildComponents(intervals);
+
+        // for each component, merge all intervals into one interval.
+        List<Interval> merged = new LinkedList<>();
+        for (int comp = 0; comp < nodesInComp.size(); comp++) {
+            merged.add(mergeNodes(nodesInComp.get(comp)));
+        }
+
+        return merged;
+    }
 }
