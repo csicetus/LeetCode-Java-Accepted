1810f5d03a654df009af165954091b05223b1058
csicetus <1577840083@qq.com>
2019-06-27 21:08:49 -0400

    Update 70_Climbing_Stairs.java

--- Code/70_Climbing_Stairs.java
+++ Code/70_Climbing_Stairs.java
@@ -123,15 +123,53 @@ class Solution {
 /*
  * Solution 5
  * 2019-06-27  Runtime: 0 ms
- * Algorithm: => Formulation
+ * Algorithm: => Binets Method
  * Time complexity: O(logn) Space complexity: O(1)
  */
  
+class Solution {
+    public int climbStairs(int n) {
+        int[][] q = {{1, 1}, {1, 0}};
+        int[][] res = pow(q, n);
+        return res[0][0];
+    }
+    
+    public int[][] pow(int[][] a, int n) {
+        int[][] ret = {{1, 0}, {0, 1}};
+        while (n > 0) {
+            // if the last bit is 1, add to the result of multiply
+            if ((n & 1) == 1) {
+                ret = multiply(ret, a);
+            }
+            n >>= 1;    // n shifts one bit to the right
+            a = multiply(a, a);     // update a
+        }
+        return ret;
+    }
+    
+    public int[][] multiply(int[][] a, int[][] b) {
+        int[][] c = new int[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j];
+            }
+        }
+        return c;
+    }
+}
+
  
  /*
  * Solution 6
  * 2019-06-27  Runtime: 0 ms
- * Algorithm: => Formulation
+ * Algorithm: => Fibonacci Formula
  * Time complexity: O(logn) Space complexity: O(1)
  */
 
+class Solution {
+    public int climbStairs(int n) {
+        double sqrt5 = Math.sqrt(5);
+        double fibn = Math.pow((1 + sqrt5) / 2, n + 1) - Math.pow((1 - sqrt5) / 2, n + 1);
+        return (int)(fibn/sqrt5);
+    }
+}
