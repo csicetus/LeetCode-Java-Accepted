68c8696ccad2f0d934fac59ebfa2e731c8ed03d4
csicetus <1577840083@qq.com>
2019-07-09 06:22:47 -0400

    Update 14_Longest_Common_Prefix.java

--- Code/14_Longest_Common_Prefix.java
+++ Code/14_Longest_Common_Prefix.java
@@ -48,3 +48,39 @@ class Solution {
         return prefix;
     }
 }
+
+
+/*
+ * Solution 3
+ * 2019-07-09  Runtime: 1 ms
+ * Algorithm: => Recursion.
+ * Time complexity: O(S), where S is the sum of all characters in all strings. Space complexity: O(m*logn).
+ */
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        return longestCommonPrefix(strs, 0, strs.length - 1);
+    }
+    
+    private String longestCommonPrefix(String[] strs, int l, int r) {
+        if (l == r) {
+            return strs[l];
+        } else {
+            int mid = (l + r) / 2;
+            String leftLCP = longestCommonPrefix(strs, l, mid);
+            String rightLCP = longestCommonPrefix(strs, mid + 1, r);
+            return commonPrefix(leftLCP, rightLCP);
+        }
+    }
+    
+    private String commonPrefix(String l, String r) {
+        int minLen = Math.min(l.length(), r.length());
+        for (int i = 0; i < minLen; i++) {
+            if (l.charAt(i) != r.charAt(i)) {
+                return l.substring(0, i);
+            }
+        }
+        return l.substring(0, minLen);
+    }
+}
