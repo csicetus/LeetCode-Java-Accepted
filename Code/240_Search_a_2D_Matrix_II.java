f1566cf43c5593be4331b868a0edff4a66bd82c7
csicetus <1577840083@qq.com>
2019-07-16 04:02:28 -0400

    Create 240_Search_a_2D_Matrix_II.java

---
+++ Code/240_Search_a_2D_Matrix_II.java
@@ -0,0 +1,35 @@
+/*
+ * 240. Search a 2D Matrix II
+ * Target: Write an efficient algorithm that searches for a value in an m x n matrix.
+ *         Integers in each row are sorted in ascending from left to right.
+*          Integers in each column are sorted in ascending from top to bottom.
+ * Difficulty：Medium
+ * Classification：Binary Search, Divide and Conquer
+ */
+
+/*
+ * Solution 1
+ * 2019-07-16  Runtime: 5 ms
+ * Algorithm: => Two points with reverse direction
+ * Time Complexity: O(n), Space Conplexity: O(1)
+ */
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0 || matrix == null) {
+            return false;
+        }
+        int m = matrix.length, n = matrix[0].length;
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+}
