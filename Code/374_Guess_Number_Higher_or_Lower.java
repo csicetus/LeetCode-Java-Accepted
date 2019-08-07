7048e06232de7755c35de290d952ed2f12aa2df7
csicetus <1577840083@qq.com>
2019-08-06 20:50:28 -0400

    Update 374_Guess_Number_Higher_or_Lower.java

--- Code/374_Guess_Number_Higher_or_Lower.java
+++ Code/374_Guess_Number_Higher_or_Lower.java
@@ -26,7 +26,60 @@
 
 /*
  * Solution 2
- * 2019-08-06  Runtime: 21 ms
- * Algorithm: => Brute Force.
- * Time Complexity: O(n). Space Conplexity: O(1). 
+ * 2019-08-06  Runtime: 0 ms
+ * Algorithm: => Binary Search.
+ * Time Complexity: O(logn). Space Conplexity: O(1). 
  */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (guess(mid) == 0) {
+                return mid;
+            }
+            else if (guess(mid) == 1) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
+
+
+/*
+ * Solution 3
+ * 2019-08-06  Runtime: 0 ms
+ * Algorithm: => Ternary Search.
+ * Time Complexity: O(log_3 n). Space Conplexity: O(1). 
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid1 = l + (r - l) / 3;
+            int mid2 = r - (r - l) / 3;
+            if (guess(mid1) == 0) {
+                return mid1;
+            }
+            if (guess(mid2) == 0) {
+                return mid2;
+            }
+            else if (guess(mid2) == 1) {
+                l = mid2 + 1;
+            }
+            else if (guess(mid1) == -1) {
+                r = mid1 - 1;
+            } else {
+                l = mid1 + 1;
+                r = mid2 - 1;
+            }
+        }
+        return n;
+    }
+}
