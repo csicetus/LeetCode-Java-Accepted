951dc8925512ea075dc8427f679f16db4edaf899
csicetus <1577840083@qq.com>
2019-10-10 23:21:36 -0400

    Update 131_Palindrome_Partitioning.java

--- Code/131_Palindrome_Partitioning.java
+++ Code/131_Palindrome_Partitioning.java
@@ -52,3 +52,47 @@
         return true;
     }
 }
+
+
+/*
+ * Solution 1
+ * 2019-10-10  Runtime: 3 ms
+ * Algorithm: => Backtracking and dp.
+ * Time Complexity: ?, Space Conplexity: O(1)
+ */
+ 
+class Solution {
+    public List<List<String>> partition(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                // In dp[i + 1][j - 1], i + 1 <= j - 1, and j = i + len - 1. So len >= 3.
+                // When len = 1 or 2, we just need to check s.charAt(i) == s.charAt(j)
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (len < 3 || dp[i + 1][j - 1]);
+            }
+        }
+        return partitionRecursion(s, 0, dp);
+    }
+    
+    private List<List<String>> partitionRecursion(String s, int start, boolean[][] dp) {
+        if (start == s.length()) {
+            List<String> list = new ArrayList<>();
+            List<List<String>> res = new ArrayList<>();
+            res.add(list);
+            return res;
+        }
+        List<List<String>> res = new ArrayList<>();
+        for (int i = start; i < s.length(); i++) {
+            if (dp[start][i]) {
+                String leftPart = s.substring(start, i + 1);
+                for (List<String> list : partitionRecursion(s, i + 1, dp)) {
+                    list.add(0, leftPart);
+                    res.add(list);
+                }
+            }
+        }
+        return res;
+    }
+}
