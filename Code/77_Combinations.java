da3c9676645540a2576663f4e7418f62e5f16cf6
csicetus <1577840083@qq.com>
2019-10-11 15:30:11 -0400

    Update 77_Combinations.java

--- Code/77_Combinations.java
+++ Code/77_Combinations.java
@@ -39,7 +39,7 @@
 
 /*
  * Solution 2
- * 2019-10-11  Runtime: 2 ms
+ * 2019-10-11  Runtime: 28 ms
  * Algorithm: => Iteration. 
  * Time Complexity: ?, Space Conplexity: O(1)
  */
@@ -77,3 +77,113 @@
     }
 }
  
+
+/*
+ * Solution 3
+ * 2019-10-11  Runtime: 7 ms
+ * Algorithm: => Iteration. Find results that length is 1. Then add other possible nums after orginal lists.
+ * Time Complexity: ?, Space Conplexity: O(1)
+ */
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        
+        List<List<Integer>> res = new ArrayList<>();
+        // results that length is 1.
+        for (int i = 1; i <= n + 1 - k; i++) {
+            res.add(Arrays.asList(i));
+        }
+        
+        // add other possible nums
+        // i stands for list length
+        for (int i = 2; i <= k; i++) {
+            List<List<Integer>> temp = new ArrayList<>();
+            /*System.out.println(i);*/
+            for (List<Integer> list : res) {
+                for (int j = list.get(list.size() - 1) + 1; j <= n + 1 - (k - (i - 1)); j++) {
+                    // read the original list and add new possible num j
+                    List<Integer> tmp = new ArrayList<>(list);
+                    tmp.add(j);
+                    /*System.out.println(tmp);*/
+                    // add new list to temp result
+                    temp.add(tmp);
+                }
+            }
+            // update result, for example, update [1] to [1, 2] for future add 3 => [1,2,3]
+            res = temp;
+        }
+        return res;
+    }
+}
+
+
+/*
+ * Solution 4
+ * 2019-10-11  Runtime: 50 ms
+ * Algorithm: => Recusion. C(k, n) = C(k - 1, n - 1) + C(k, n - 1)
+ *               For num n, if it is selected, then we need find k - 1 nums in n - 1. If not, then we need find k nums in n - 1.
+ * Time Complexity: ?, Space Conplexity: O(1)
+ */
+
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        if (k == 0 || n == k) {
+            List<Integer> list = new ArrayList<>();
+            for (int i = 1; i <= k; i++) {
+                list.add(i);
+            }
+            return new ArrayList<>(Arrays.asList(list));
+        }
+        List<List<Integer>> res = combine(n - 1, k - 1);
+        
+        res.forEach(e -> e.add(n));
+        res.addAll(combine(n - 1, k));
+        
+        return res;
+    }
+}
+
+
+/*
+ * Solution 5
+ * 2019-10-11  Runtime: 85 ms
+ * Algorithm: => DP. Change sol 4 recursion to DP
+ *               For num n, if it is selected, then we need find k - 1 nums in n - 1. If not, then we need find k nums in n - 1.
+ * Time Complexity: ?, Space Conplexity: O(1)
+ */
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> dp[][] = new List[n + 1][k + 1];
+        // if k == 0, return empty list
+        for (int m = 0; m <= n; m++) {
+            dp[m][0] = new ArrayList<>();
+            dp[m][0].add(new ArrayList<Integer>());
+        }
+        // row from 1 to n
+        for (int i = 1; i <= n; i++) {
+            // col from 1 to k and col <= row
+            for (int j = 1; j <= i && j <= k; j++) {
+                dp[i][j] = new ArrayList<>();
+                // if we didn't select i, then need to select total j nums.
+                // if we can select j nums from i - 1
+                // so we have j <= i - 1 => j < i
+                if (j < i) {
+                    dp[i][j].addAll(dp[i - 1][j]);
+                }
+                // if we select i,
+                // then select j - 1 nums from i - 1
+                for (List<Integer> list : dp[i - 1][j - 1]) {
+                    // obtain the result of selecting j - 1 nums from i - 1
+                    // then fro each result add i because we selected i
+                    List<Integer> tmp = new ArrayList<>(list);
+                    tmp.add(i);
+                    // add updated result to dp[i][j]
+                    dp[i][j].add(tmp);
+                }
+            }
+        }
+        return dp[n][k];
+    }
+}
