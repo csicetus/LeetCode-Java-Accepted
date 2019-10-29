cea56cae3b7c4be8cf3fbe42913dd7982f141692
csicetus <1577840083@qq.com>
2019-10-29 16:13:47 -0400

    Create 113_Path_Sum_II.java

---
+++ Code/113_Path_Sum_II.java
@@ -0,0 +1,75 @@
+/*
+ * 113. Path Sum II
+ * Target: Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+ *         Note: A leaf is a node with no children.
+ * Difficulty：Medium
+ * Classification：Tree, DFS
+ */
+
+/*
+ * Solution 1
+ * 2019-10-29  Runtime: 1 ms
+ * Algorithm: => Recursion
+ * Time Complexity: ?, Space Conplexity: O(1)
+ */
+ 
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        pathSumHelper(root, sum, temp, res);
+        return res;
+    }
+    
+    private void pathSumHelper(TreeNode cur, int sum, List<Integer> temp, List<List<Integer>> res) {  
+        if (cur.left == null && cur.right == null) {
+            if (cur.val == sum) {
+                temp.add(cur.val);
+                res.add(new ArrayList<>(temp));
+                temp.remove(temp.size() - 1);
+            }  
+            return;
+        }
+        if (cur.right == null) {
+            temp.add(cur.val);
+            pathSumHelper(cur.left, sum - cur.val, temp, res);
+            temp.remove(temp.size() - 1);
+            return;
+            
+        }
+        if (cur.left == null) {
+            temp.add(cur.val);
+            pathSumHelper(cur.right, sum - cur.val, temp, res);
+            temp.remove(temp.size() - 1);
+            return;
+        }
+        temp.add(cur.val);
+        pathSumHelper(cur.left, sum - cur.val, temp, res);
+        temp.remove(temp.size() - 1);
+        
+        temp.add(cur.val);
+        pathSumHelper(cur.right, sum - cur.val, temp, res);
+        temp.remove(temp.size() - 1);
+        return;
+    }
+}
+
+
+/*
+ * Solution 2
+ * 2019-10-29  Runtime: 1 ms
+ * Algorithm: => DFS
+ * Time Complexity: ?, Space Conplexity: O(1)
+ */
