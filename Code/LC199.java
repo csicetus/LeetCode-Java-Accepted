e8aee92014add2616d30aef6675d47af6c68439d
csicetus <1577840083@qq.com>
2019-12-22 16:52:54 -0500

    Update LC199.java

--- Code/LC199.java
+++ Code/LC199.java
@@ -35,3 +35,34 @@ class Solution {
         return res;
     }
 }
+
+
+//////////sol 2
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        helper(root, res, 0);
+        return res;
+    }
+    
+    private void helper(TreeNode root, List<Integer> res, int level) {
+        if (root == null) {
+            return;
+        }
+        if (level == res.size()) {
+            res.add(root.val);
+        }
+        helper(root.left, res, level + 1);
+        helper(root.right, res, level + 1);
+    }
+}
