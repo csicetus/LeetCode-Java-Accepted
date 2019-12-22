d99e5e4beabfdd4fe99cd0f7054c24e8f0858b3b
csicetus <1577840083@qq.com>
2019-12-22 16:31:51 -0500

    Create LC199.java

---
+++ Code/LC199.java
@@ -0,0 +1,37 @@
+//////////sol 1
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int level = q.size();
+            for (int i = 0; i < level; i++) {
+                TreeNode cur = q.poll();
+                if (i == level - 1) {
+                    res.add(cur.val);
+                }
+                if (cur.left != null) {
+                    q.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    q.offer(cur.right);
+                }
+            }
+        }
+        return res;
+    }
+}
