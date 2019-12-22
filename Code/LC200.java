17f58f08cb2e8f7843eb7de10b2c5de3b3f6e88b
csicetus <1577840083@qq.com>
2019-12-22 00:35:37 -0500

    Update LC200.java

--- Code/LC200.java
+++ Code/LC200.java
@@ -33,3 +33,84 @@ class Solution {
 
 //////////sol 2
 
+class UnionFind {
+    int pre[];
+    int num;
+    
+    public UnionFind(int row, int col, char[][] grid) {
+        num = 0;
+        // calculate grid[i][j] == '1' number
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    num++;
+                }
+            }
+        }
+        
+        // set pre[]
+        int total = row * col;
+        pre = new int[total];
+        // firstly each node's parent is itself
+        for (int i = 0; i < total; i++) {
+            pre[i] = i;
+        }
+    }
+    
+    int find(int node) {
+        // while current not root, go upstairs to find root
+        while (pre[node] != node) {
+            pre[node] = pre[pre[node]];
+            node = pre[node];
+        }
+        return node;
+    }
+    
+    void union(int node1, int node2) {
+        // find two groups' roots
+        int root1 = find(node1);
+        int root2 = find(node2);
+        // if groups' roots not equal, union them
+        if (root1 != root2) {
+            pre[root2] = root1;
+            num--;
+        }
+    }
+    
+    int getNum() {
+        return num;
+    }
+}
+
+class Solution {
+    int row;
+    int col;
+    
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0) {
+            return 0;
+        }
+        row = grid.length;
+        col = grid[0].length;
+        UnionFind uf = new UnionFind(row, col, grid);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    // refresh right and bottom num
+                    if (i != (row - 1) && grid[i + 1][j] == '1') {
+                        uf.union(node(i, j), node(i + 1, j));
+                    }
+                    if (j != (col - 1) && grid[i][j + 1] == '1') {
+                        uf.union(node(i, j), node(i, j + 1));
+                    }
+                }
+            }
+        }
+        return uf.getNum();
+    }
+    
+    int node(int i, int j) {
+        return i * col + j;
+    }
+}
+
