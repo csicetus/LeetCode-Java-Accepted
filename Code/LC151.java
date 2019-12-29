14e9754b759ab5cd865be04bf5f4468b21076e96
csicetus <1577840083@qq.com>
2019-12-29 12:25:32 -0500

    Update LC151.java

--- Code/LC151.java
+++ Code/LC151.java
@@ -25,3 +25,67 @@ class Solution {
         return res.toString();
     }
 }
+
+
+////////// sol 2
+
+class Solution {
+    public String reverseWords(String s) {
+        if (s == "") return s;
+        char chs[] = s.toCharArray();
+        int n = chs.length;
+        reverse(chs, 0, n - 1);
+        reverseWord(chs, n);
+        return cleanSpace(chs, n);
+    }
+    
+    private void reverse(char[] chs, int i, int j) {
+        while (i < j) {
+            char c = chs[i];
+            chs[i++] = chs[j];
+            chs[j--] = c;
+        }
+    }
+    
+    private void reverseWord(char[] chs, int n) {
+        int i = 0, j = 0;
+        while (i < n) {
+            // skip space
+            while (i < j || i < n && chs[i] == ' ') {
+                i++;
+            }
+            // skip no space
+            while (j < i || j < n && chs[j] != ' ') {
+                j++;
+            }
+            reverse(chs, i, j - 1);
+            //System.out.println("======" + i + " " + j);
+        }
+    }
+    
+    private String cleanSpace(char[] chs, int n) {
+        int i = 0, j = 0;
+        for (char c : chs) {
+            System.out.println(c);
+        }
+        
+        while (j < n) {
+            // skip space
+            while (j < n && chs[j] == ' ') {
+                j++;
+            }
+            // keep no space
+            while (j < n && chs[j] != ' ') {
+                chs[i++] = chs[j++];
+            }
+            // skip space
+            while (j < n && chs[j] == ' ') {
+                j++;
+            }
+            if (j < n) {
+                chs[i++] = ' ';
+            }
+        }
+        return new String(chs).substring(0, i);
+    }
+}
