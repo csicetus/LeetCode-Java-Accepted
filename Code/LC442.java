ed0daa68d0aae4c0801909318751287eaf93f961
csicetus <1577840083@qq.com>
2020-01-06 14:05:00 -0500

    Update LC442.java

--- Code/LC442.java
+++ Code/LC442.java
@@ -5,6 +5,7 @@ class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> res = new ArrayList<Integer>();
         for (int i = 0; i < nums.length; i++) {
+            // duplicate num has same index
             int index = Math.abs(nums[i]) - 1;
             // System.out.println("index: "+index);
             // System.out.println("num[index]: "+nums[index]);
