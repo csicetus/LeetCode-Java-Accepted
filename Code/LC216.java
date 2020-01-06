cd2545313653178bc3465c4b11a69020f7a0f35f
csicetus <1577840083@qq.com>
2020-01-05 22:04:31 -0500

    Create LC216.java

---
+++ Code/LC216.java
@@ -0,0 +1,22 @@
+/////////// sol 1: backtrack
+
+// Time: O(n*2^n)?; Space: O(2^n)?
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (k == 0 || n == 0) return res;
+        backtrack(k, n, new ArrayList<Integer>(), res, 1);
+        return res;
+    }
+    
+    private void backtrack(int k, int n, List<Integer> tempList, List<List<Integer>> res, int start) {
+        if (tempList.size() == k && n == 0) {
+            res.add(new ArrayList<Integer>(tempList));
+        }
+        for (int i = start; i <= 9 && i <= n; i++) {
+            tempList.add(i);
+            backtrack(k, n - i, tempList, res, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
