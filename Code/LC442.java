e3a3c572d00dd6e463f6d4d3ceb6e01de1f5b8f3
csicetus <1577840083@qq.com>
2020-01-06 12:07:37 -0500

    Create LC442.java

---
+++ Code/LC442.java
@@ -0,0 +1,28 @@
+////////// sol 1: use the way setting num negative as boolean
+
+// Time: O(n); Space: O(1)
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            // System.out.println("index: "+index);
+            // System.out.println("num[index]: "+nums[index]);
+            if (nums[index] < 0) {
+                //System.out.println("index in res: "+index);
+                res.add(Math.abs(index + 1));
+            }
+            nums[index] = -nums[index];
+            
+            // for (int n : nums) {
+            //     System.out.print(n + " ");
+            // }
+            // System.out.println("");
+            // for (int r : res) {
+            //     System.out.print("res "+r + " ");
+            // }
+            // System.out.println("");
+        }
+        return res;
+    }
+}
