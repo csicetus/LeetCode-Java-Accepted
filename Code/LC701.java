8286e2f998c18a63954db51331024b6bba7c9a42
csicetus <1577840083@qq.com>
2020-01-08 15:03:24 -0500

    Create LC701.java

---
+++ Code/LC701.java
@@ -0,0 +1,53 @@
+//////////////// sol 1: Recursive
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
+
+//////////////// sol 2: Iterative
+
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        TreeNode curr = root;
+        while (true) {
+            if (val < curr.val) {
+                if (curr.left != null) {
+                    curr = curr.left;
+                } else {
+                    curr.left = new TreeNode(val);
+                    break;
+                }
+            } else {
+                if (curr.right != null) {
+                    curr = curr.right;
+                } else {
+                    curr.right = new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+}
