0aa5da99e8f7acc093981c3c38529e11a8a31bf9
csicetus <1577840083@qq.com>
2020-01-16 09:16:03 -0500

    Update LC121.java

--- Code/LC121.java
+++ Code/LC121.java
@@ -15,3 +15,22 @@ class Solution {
         return maxprofit;
     }
 }
+
+
+//////////////// sol 2: one  pass (stock series)
+
+// Time: O(n); Space: O(1)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int T_i10 = 0;
+        int T_i11 = Integer.MIN_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            // T_i10 = max(T_i10, T_i11 + prices[i]);
+            T_i10 = Math.max(T_i10, T_i11 + prices[i]);
+            // T_i11 = max(T_i11, T_i00 - prices[i]);
+            // T_i00 = 0
+            T_i11 = Math.max(T_i11, -prices[i]);
+        }
+        return T_i10;
+    }
+}
