1fb77bc80cc93aa3263615f6cc93c8dc1b83ae24
csicetus <1577840083@qq.com>
2020-02-20 20:51:08 -0500

    Create LC383.java

---
+++ Code/LC383.java
@@ -0,0 +1,19 @@
+// array map: int[26]
+
+// Time: O(n), n is length of magazine
+// Space: O(1)
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            arr[magazine.charAt(i) - 'a']++;
+        }
+        
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (--arr[ransomNote.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
